16a951e5758f0757cf52004cfddacab3b003cf30
Mulaudzi Zwidofhelangani <mulaudzizwido7@gmail.com>
2025-09-15 00:06:10 +0200

    Define Sudoku constructor and destructor

--- Sudoku.cpp
+++ Sudoku.cpp
@@ -3,6 +3,34 @@
 //
 
 #include "Sudoku.h"
+#include <random>
+
+Sudoku::Sudoku() {
+    board = new std::string *[9];
+    boardValues = new std::string *[9];
+
+    for (int i = 0; i < 9; ++i) {
+        board[i] = new std::string[9];
+        boardValues[i] = new std::string[9];
+    }
+
+}
+
+Sudoku::~Sudoku() {
+
+    for (int i = 0; i < 9; ++i) {
+        delete [] board[i];
+        board[i] = nullptr;
+        delete [] boardValues[i];
+        boardValues[i] = nullptr;
+    }
+
+    delete [] board;
+    board = nullptr;
+    delete [] boardValues;
+    boardValues = nullptr;
+}
+
 
 void Sudoku::printBoard(sf::RenderWindow &mainW) {
     sf::RenderWindow window(sf::VideoMode{800, 800}, "Sudoku");
--- Sudoku.h
+++ Sudoku.h
@@ -6,9 +6,17 @@
 #define SUDOKU_SUDOKU_H
 
 #include "Button.h"
+#include <vector>
+#include <string>
 
 class Sudoku {
+private:
+    std::string** board;
+    std::string** boardValues;
+    std::vector<sf::RectangleShape> blocks;
 public:
+    Sudoku();
+    ~Sudoku();
     void printBoard(sf::RenderWindow& mainW);
 };
 
